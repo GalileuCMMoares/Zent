1735737b44e4164b62be346a5bf4b266248d3607
GalileuCMMoares <galileumcalaca@gmail.com>
2026-01-27 20:55:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/zent/presentation/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.zent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.zent.ui.theme.ZentTheme
+import com.zent.app.presentation.MainScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ZentTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/zent/viewmodel/ZentViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.zent.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ZentViewModel : ViewModel() {
+
+}
